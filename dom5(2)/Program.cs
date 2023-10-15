679aac2d99389236d295d571641d2f025db607ec
Арман <@LAPTOP-8BLD2IN5>
2023-10-15 22:59:39 +0600

    Добавьте файлы проекта.

---
+++ dom5(2)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        try
+        {
+            for (int i = 0; i <= array.Length; i++)
+            {
+                int element = array[i];
+                Console.WriteLine($"Элемент {i}: {element}");
+            }
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine("Произошла ошибка: выход за пределы массива.");
+        }
+        finally
+        {
+            Console.WriteLine("Завершение обработки массива.");
+        }
+    }
+}
